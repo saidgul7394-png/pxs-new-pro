448195d175fe9d7aa8141af74245419d5538da84
saidgul7394-png <saidgul7394@gmail.com>
2026-07-14 00:24:50 -0700

    Add files via upload

---
+++ flutter_src/android/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# Proguard rules for PSX Vision Pro
+# Add any custom rules here to keep classes from obfuscation or shrinking
---
+++ flutter_src/android/app/src/main/kotlin/com/psxvision/pro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.psxvision.pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
